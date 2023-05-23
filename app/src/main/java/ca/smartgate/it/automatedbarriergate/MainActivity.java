78137016573d727f5566f36f6d368b58d02a2eb6
richard <richardpancham777@gmail.com>
2023-05-23 13:57:02 -0400

    Initial commit

---
+++ app/src/main/java/ca/smartgate/it/automatedbarriergate/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.smartgate.it.automatedbarriergate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
