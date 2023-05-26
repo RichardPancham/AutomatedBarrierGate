f5c7e84a0c533b92af5b59eb07c97a9e80b18e1f
richard <richardpancham777@gmail.com>
2023-05-26 13:31:45 -0400

    implemented functionality for bottom navigation and swithing between fragment

--- AutoBarrierGate/src/main/java/ca/smartgate/it/automatedbarriergate/MainActivity.java
+++ AutoBarrierGate/src/main/java/ca/smartgate/it/automatedbarriergate/MainActivity.java
@@ -1,14 +1,53 @@
 package ca.smartgate.it.automatedbarriergate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private Fragment homeFragment;
+    private Fragment locationFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeFragment = new HomeFragment();
+        locationFragment = new LocationFragment();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        switchFragment(homeFragment);
+                        return true;
+                    case R.id.location:
+                        switchFragment(locationFragment);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        // Set the initial fragment
+        switchFragment(homeFragment);
+    }
+
+    private void switchFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment);
+        fragmentTransaction.commit();
     }
 }
