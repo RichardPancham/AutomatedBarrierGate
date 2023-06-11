f33bf77d73531019a98a42025cc205a41a8f4138
jaspreetheer <jaspreetheer2017@gmail.com>
2023-06-11 14:30:34 -0400

    Commit 10 - Jaspreet Heer
    Added an image to the alertdialog for back key press, added a prompt for location permission request.

--- AutoBarrierGate/src/main/java/ca/smartgate/it/automatedbarriergate/MainActivity.java
+++ AutoBarrierGate/src/main/java/ca/smartgate/it/automatedbarriergate/MainActivity.java
@@ -5,15 +5,22 @@ package ca.smartgate.it.automatedbarriergate;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.Manifest;
+
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -23,6 +30,7 @@ public class MainActivity extends AppCompatActivity{
     private  Fragment barrierFragment;
 
     private static final int SPLASH_DELAY = 2000; // Time in milliseconds
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +38,21 @@ public class MainActivity extends AppCompatActivity{
 
 
         setContentView(R.layout.activity_main);
+
+        // Check if location permission is granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Location permission not granted, request it
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            // Location permission already granted, start using location
+            startLocationUpdates();
+        }
+
+
+
 
         paymentFragment = new PaymentFragment();
         locationFragment = new LocationFragment();
@@ -62,6 +85,27 @@ public class MainActivity extends AppCompatActivity{
 
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Location permission granted, start using location
+                startLocationUpdates();
+            } else {
+                // Location permission denied, handle the situation
+                Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void startLocationUpdates() {
+        // Location permission granted, start using location
+        // Implement your location-related code here
+    }
     // Inflate the menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -91,6 +135,10 @@ public class MainActivity extends AppCompatActivity{
         builder.setMessage(R.string.ExitPrompt);
         builder.setCancelable(false);
 
+        //set image for AlertDialog
+        ImageView imageView = new ImageView(this);
+        imageView.setImageResource(R.drawable.android);
+        builder.setView(imageView);
         builder.setPositiveButton(R.string.Yes, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
