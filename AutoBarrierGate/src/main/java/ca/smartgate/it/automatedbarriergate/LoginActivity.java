ac640c2db00b3bc6e45e353200da7aecfe5e9b62
richard <richardpancham777@gmail.com>
2023-06-11 18:20:58 -0400

    removed menu from login screen

--- AutoBarrierGate/src/main/java/ca/smartgate/it/automatedbarriergate/LoginActivity.java
+++ AutoBarrierGate/src/main/java/ca/smartgate/it/automatedbarriergate/LoginActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LoginActivity extends MainMenu{
+public class LoginActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
