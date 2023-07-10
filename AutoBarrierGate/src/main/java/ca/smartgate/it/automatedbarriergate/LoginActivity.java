d9aed671aa03b52de085050eea5891e5fc1a5c48
jaspreetheer <jaspreetheer2017@gmail.com>
2023-07-10 19:46:58 -0400

    Commit 12 - Jaspreet Heer
    Added verify credentials method to verify credentials stored in database. Logic still being coded.

--- AutoBarrierGate/src/main/java/ca/smartgate/it/automatedbarriergate/LoginActivity.java
+++ AutoBarrierGate/src/main/java/ca/smartgate/it/automatedbarriergate/LoginActivity.java
@@ -18,20 +18,32 @@ public class LoginActivity extends AppCompatActivity{
     private Button loginButton;
     private FirebaseFirestore db;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        emailEditText = findViewById(R.id.editTextTextEmailAddress);
+        passwordEditText = findViewById(R.id.editTextTextPassword);
+        loginButton = findViewById(R.id.button);
+
+        db = FirebaseFirestore.getInstance();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Start the SecondActivity
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+                verifyCredentials(email, password);
             }
         });
 
+    }
+
+    private void verifyCredentials(final String email, final String password) {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
