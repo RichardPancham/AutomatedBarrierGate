a917dbb149e1c86cc79f640030ca4bceeb89c620
jaspreetheer <jaspreetheer2017@gmail.com>
2023-07-10 19:40:49 -0400

    Commit 11 - Jaspreet Heer
    Adding login verification integration with Firebase.

--- AutoBarrierGate/src/main/java/ca/smartgate/it/automatedbarriergate/LoginActivity.java
+++ AutoBarrierGate/src/main/java/ca/smartgate/it/automatedbarriergate/LoginActivity.java
@@ -6,8 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity{
+
+    private EditText emailEditText, passwordEditText;
+    private Button loginButton;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
