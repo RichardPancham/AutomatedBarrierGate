a0880ccea657b6bcd5ae900869fe0d4eca2658f7
jaspreetheer <jaspreetheer2017@gmail.com>
2023-07-15 11:28:19 -0400

    Commit 16 - Jaspreet Heer
    Integrated database for the review screen. Name, star rating, and review are all pushed to the database now.

--- AutoBarrierGate/src/main/java/ca/smartgate/it/automatedbarriergate/ReviewsFragment.java
+++ AutoBarrierGate/src/main/java/ca/smartgate/it/automatedbarriergate/ReviewsFragment.java
@@ -1,4 +1,5 @@
 package ca.smartgate.it.automatedbarriergate;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,15 +15,33 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ReviewsFragment extends Fragment {
     private EditText editTextReview;
+    private EditText editTextName;
     private Button buttonSubmit;
-    private ListView listViewReviews;
-    private List<String> reviewsList;
-    private ArrayAdapter<String> reviewsAdapter;
 
+    private ArrayAdapter<String> reviewsAdapter;
+    private FirebaseFirestore db;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get a reference to the Firebase Realtime Database
+        db = FirebaseFirestore.getInstance();
+    }
+
+    @SuppressLint("MissingInflatedId")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -31,18 +50,37 @@ public class ReviewsFragment extends Fragment {
         // Initialize UI components
         RatingBar ratingBar = rootView.findViewById(R.id.ratingBar);
         editTextReview = rootView.findViewById(R.id.editTextReview);
+        editTextName=rootView.findViewById(R.id.editTextName);
         buttonSubmit = rootView.findViewById(R.id.buttonSubmit);
 
         // Submit button click listener
         buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String review = editTextReview.getText().toString();
-                if (review.isEmpty()) {
-                    Toast.makeText(getActivity(), "Feel free to write a review", Toast.LENGTH_SHORT).show();
-                }
+                String review1 = editTextReview.getText().toString();
                 float rating = ratingBar.getRating();
-                Toast.makeText(getActivity(), "Thank you for your review!", Toast.LENGTH_SHORT).show();
+                String name = editTextName.getText().toString();
+
+                //Database Review Push
+                Map<String, Object> reviewData = new HashMap<>();
+                reviewData.put("rating", rating);
+                reviewData.put("review", review1);
+                reviewData.put("name", name);
+
+                db.collection("reviews").add(reviewData)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(getActivity(), "Thank you for your review!", Toast.LENGTH_LONG).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getActivity(), "Failed to submit review.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                //Toast.makeText(getActivity(), "Thank you for your review!", Toast.LENGTH_SHORT).show();
             }
         });
 
