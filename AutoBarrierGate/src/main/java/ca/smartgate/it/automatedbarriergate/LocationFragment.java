094a40e04a927bf02e6514a528dc42c33ba7201d
jaspreetheer <jaspreetheer2017@gmail.com>
2023-07-23 20:49:33 -0400

    Commit 23 - Jaspreet Heer
    Added control over which parking spots/radio buttons can be selected. Just need to integrate database so it pulls from the database. Currently it's reading manually selected values.

--- AutoBarrierGate/src/main/java/ca/smartgate/it/automatedbarriergate/LocationFragment.java
+++ AutoBarrierGate/src/main/java/ca/smartgate/it/automatedbarriergate/LocationFragment.java
@@ -25,6 +25,7 @@ public class LocationFragment extends Fragment {
 
     private FirebaseFirestore db;
     private RadioGroup radioGroup;
+    private RadioButton radio1, radio2, radio3, radio4, radio5;
     private Button selectButton;
 
     private TimePicker timePicker;
@@ -46,45 +47,54 @@ public class LocationFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_location, container, false);
 
-        numberPicker = view.findViewById(R.id.numberPicker);
-        radioGroup = view.findViewById(R.id.radioGroup);
+
         selectButton = view.findViewById(R.id.button7);
-        timePicker = view.findViewById(R.id.timePicker);
+        // Get references to RadioGroup and RadioButtons
+        radioGroup = view.findViewById(R.id.radioGroup);
+        radio1 = view.findViewById(R.id.radioButton);
+        radio2 = view.findViewById(R.id.radioButton2);
+        radio3 = view.findViewById(R.id.radioButton3);
+        radio4 = view.findViewById(R.id.radioButton4);
+        radio5 = view.findViewById(R.id.radioButton5);
 
-        //set min and max values for numberpicker
-        numberPicker.setMinValue(1);
-        numberPicker.setMaxValue(12);
-        numberPicker.setValue(1);
 
-        // Set a listener to update the selected number TextView
-        numberPicker.setOnValueChangedListener((picker, oldVal, newVal) -> {
-            x=newVal;
+        int p1=1;
+        int p2=1;
+        int p3=1;
+        int p4=0;
+        int p5=0;
+        // Set listener to the RadioGroup to handle the check behavior
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.radioButton && p1 == 0) {
+                    radio1.setChecked(false);
+                } else if (checkedId == R.id.radioButton2 && p2 == 0) {
+                    radio2.setChecked(false);
+                } else if (checkedId == R.id.radioButton3 && p3 == 0) {
+                    radio3.setChecked(false);
+                } else if (checkedId == R.id.radioButton4 && p4 == 0) {
+                    radio4.setChecked(false);
+                } else if (checkedId == R.id.radioButton5 && p5 == 0) {
+                    radio5.setChecked(false);
+                }
+            }
         });
 
 
         selectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int selectedRadioButtonId = radioGroup.getCheckedRadioButtonId();
-                if (selectedRadioButtonId != -1) {
-                    // At least one radio button is selected
-                    RadioButton selectedRadioButton = view.findViewById(selectedRadioButtonId);
-                    String selectedOption = selectedRadioButton.getText().toString();
-                    Toast.makeText(getActivity(), "Selected Option: " + selectedOption, Toast.LENGTH_SHORT).show();
-                    BottomNavigationView bottomNavigationView = requireActivity().findViewById(R.id.bottomNavigationView);
-                    bottomNavigationView.setSelectedItemId(R.id.payment);
-                } else {
-                    // No radio button is selected
-                    Toast.makeText(getActivity(), "Please select an option", Toast.LENGTH_SHORT).show();
-                }
+              //  int selectedRadioButtonId = radioGroup.getCheckedRadioButtonId();
+
 
                 //Getting Time
-                int hour = timePicker.getCurrentHour();
-                int minute = timePicker.getCurrentMinute();
+           //     int hour = timePicker.getCurrentHour();
+             //   int minute = timePicker.getCurrentMinute();
 
                 // Handle the selected time here
-                String selectedTime = hour + ":" + minute;
-                Toast.makeText(getActivity(), "Selected time: " + selectedTime, Toast.LENGTH_SHORT).show();
+               // String selectedTime = hour + ":" + minute;
+                //Toast.makeText(getActivity(), "Selected time: " + selectedTime, Toast.LENGTH_SHORT).show();
                 //openPaymentFragment(1, 2, 3);
             }
         });
