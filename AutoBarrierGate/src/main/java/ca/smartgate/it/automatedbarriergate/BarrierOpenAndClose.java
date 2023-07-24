77293ffe381c943acd3a8d380ca008558c6c8222
jaspreetheer <jaspreetheer2017@gmail.com>
2023-07-23 22:33:30 -0400

    Commit 25 - Jaspreet Heer
    Added exit beam read from database, implemented 2 pictures which show and open and closed gate. Picture will show depending on gate status.

--- AutoBarrierGate/src/main/java/ca/smartgate/it/automatedbarriergate/BarrierOpenAndClose.java
+++ AutoBarrierGate/src/main/java/ca/smartgate/it/automatedbarriergate/BarrierOpenAndClose.java
@@ -6,10 +6,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,6 +33,7 @@ import java.util.Map;
  * create an instance of this fragment.
  */
 public class BarrierOpenAndClose extends Fragment {
+    private static final int DELAY_MILLIS = 5000; // 5 seconds
     FirebaseFirestore firestore;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -73,11 +77,16 @@ public class BarrierOpenAndClose extends Fragment {
     private FirebaseFirestore db;
     private TextView textView;
     private TextView textView2;
+
+    private ImageView imageView;
+    private boolean isImage1Shown = true;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_barrier_open_and_close, container, false);
+        imageView = view.findViewById(R.id.imageView4);
 
         Button myButton = view.findViewById(R.id.button2);
         db = FirebaseFirestore.getInstance();
@@ -89,6 +98,7 @@ public class BarrierOpenAndClose extends Fragment {
             public void onClick(View v)
             {
                 fetchDataFromFirestore();
+                toggleImage();
             }
         });
 
@@ -137,7 +147,26 @@ public class BarrierOpenAndClose extends Fragment {
                         // Handle any errors that occurred during the fetching process
                     }
                 });
+        toggleImage();
+        // Simulate a 10-second delay and show a toast after the delay
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Code to be executed after the 10-second delay
+                Toast.makeText(requireContext(), "5 seconds delay completed!", Toast.LENGTH_SHORT).show();
+                toggleImage();
+            }
+        }, DELAY_MILLIS);
+        //Switch Picture back to Closed gate
+        //toggleImage();
 
     }
-
+    private void toggleImage() {
+        if (isImage1Shown) {
+            imageView.setImageResource(R.drawable.screenshot_2023_07_23_at_10_17_28_pm);
+        } else {
+            imageView.setImageResource(R.drawable.screenshot_2023_07_23_at_10_17_22_pm);
+        }
+        isImage1Shown = !isImage1Shown;
+    }
 }
